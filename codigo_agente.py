88e3619e9de5088e469e0ceca3aa4d9761233bc1
Adolfo145 <kantunadolfo94@gmail.com>
2026-09-17 19:32:10 -0600

    Add files via upload

---
+++ codigo_agente.py
@@ -0,0 +1,23 @@
+
+# Rutas disponibles
+rutas = [
+    {
+        "nombre": "Ruta A",
+        "distancia": 5,
+        "tiempo": 10,
+        "trafico": 1
+    },
+    {
+        "nombre": "Ruta B",
+        "distancia": 3,
+        "tiempo": 15,
+        "trafico": 3
+    },
+    {
+        "nombre": "Ruta C",
+        "distancia": 7,
+        "tiempo": 8,
+        "trafico": 1
+    }
+]
+
